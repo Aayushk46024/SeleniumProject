079f66a2826e400cec5d12a2753593ed2fc52197
AayushKhandelwal <aayush.khandelwal@infostride.com>
2022-01-13 19:32:04 +0530

    Second commit

--- Browser.java
+++ Browser.java
@@ -1,4 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -26,9 +28,22 @@ public class Browser {
 //        driver.navigate().refresh();
 
 
-        driver.switchTo().newWindow(WindowType.WINDOW);
-        driver.switchTo().newWindow(WindowType.TAB);
-        driver.switchTo().window(originalWindow);
+//        driver.switchTo().newWindow(WindowType.WINDOW);
+//        driver.switchTo().newWindow(WindowType.TAB);
+//        driver.switchTo().window(originalWindow);
+//        driver.navigate().to("https://www.geeksforgeeks.org/operators-in-java/");
+//        driver.switchTo().frame("packageFrame");
+//        driver.findElement(By.linkText("Arithmetic Operators")).click();
+//        driver.switchTo().defaultContent();
+
+        driver.manage().window().getSize().getWidth();
+        driver.manage().window().getSize().getHeight();
+
+        Dimension size = driver.manage().window().getSize();
+        System.out.println(size.getWidth());
+        System.out.println(size.getHeight());
+
+        driver.manage().window().setSize(new Dimension(800,600));
 
         Thread.sleep(2000);
         driver.close();
